ccab205b44a52d073175ebc433c8a3f543323dff
SabinaIldirim2000 <164672963+SabinaIldirim2000@users.noreply.github.com>
2024-06-24 04:01:39 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import databaseConnection from "./utils/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+
+databaseConnection();
+
+dotenv.config({
+    path: ".env"
+})
+
+const app = express();
+//middlewares
+app.use(express.urlencoded({ extended:true}));
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+// api
+app.use("/api/v1/user", userRoute);
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listen at port ${process.env.PORT}`);
+});
