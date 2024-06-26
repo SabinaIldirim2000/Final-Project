2ee4c24a0b57340f4d558903a500a93ed3d7440d
SabinaIldirim2000 <164672963+SabinaIldirim2000@users.noreply.github.com>
2024-06-27 03:56:39 +0400

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,46 @@
+import axios from 'axios';
 import React from 'react'
 import { IoIosArrowDropdown } from "react-icons/io";
+import { useDispatch, useSelector } from 'react-redux';
+import { API_END_POINT } from '../utils/constant';
+import { setUser } from '../redux/userSlice';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 const Header = () => {
+    const user = useSelector((store) => store.app.user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+    const logoutHandler = async () => {
+        try {
+            const res = await axios.get(`${API_END_POINT}/logout`);
+          if(res.data.success){
+            toast.success(res.data.message);
+          }
+            dispatch(setUser(null));
+            navigate("/");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className=' absolute z-10 flex w-[100vw] items-center justify-between px-6 bg-gradient-to-b from-black '>
             <img className='w-56' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png" alt="netflix-logo" />
-            <div className='flex items-center'>
-            <IoIosArrowDropdown size="24px" color='white'/>
-                <h1 className='text-lg font-medium text-white'>Patel Mernstack</h1>
-                <div className='ml-4'>
-                    <button className='bg-red-800 text-white px-4 py-2'>Logout</button>
-                    <button className='bg-red-800 text-white px-4 py-2 ml-2'>Search Movie</button>
+            {
+                user && (
+                    <div className='flex items-center'>
+                        <IoIosArrowDropdown size="24px" color='white' />
+                        <h1 className='text-lg font-medium text-white'>{user.fullName}</h1>
+                        <div className='ml-4'>
+                            <button onClick={logoutHandler} className='bg-red-800 text-white px-4 py-2'>Logout</button>
+                            <button className='bg-red-800 text-white px-4 py-2 ml-2'>Search Movie</button>
+                        </div>
+                    </div>
+                )
+            }
 
-                </div>
-            </div>
         </div>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,76 @@
 import React, { useState } from 'react'
 import Header from './Header'
+import axios from "axios";
+import { API_END_POINT } from '../utils/constant';
+import toast from "react-hot-toast"
+import {useNavigate} from "react-router-dom"
+import { useDispatch, useSelector } from 'react-redux';
+import { setLoading, setUser } from '../redux/userSlice';
+
 
 const Login = () => {
     const [isLogin, setIsLogin] = useState(false);
     const [Fullname, setFullName] = useState("");
     const [email, SetEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate ();
+    const dispatch = useDispatch();
+
+    const isLoading = useSelector(store=>store.app.isLoading);
 
     const loginHandler = () => {
         setIsLogin(!isLogin);
     }
-
-    const getInputData = (e)=>{
+    const getInputData = async (e)=>{
         e.preventDefault();
-        console.log(Fullname,email,password);
+        dispatch(setLoading(true));
+        if(isLogin){
+            //login
+            const user = {email,password};
+            try {
+                const res = await axios.post(`${API_END_POINT}/login`, user,{
+                     headers:{
+                    'Content-Type':'application/json'
+                },
+                withCredentials:true
+                });
+                if(res.data.success){
+                    toast.success(res.data.message);
+                }
+                dispatch(setUser(res.data.user));
+                navigate("/browse");
+            } catch (error) {
+                toast.error(error.response.data.message);
+                console.log(error);
+            } finally {
+                dispatch(setLoading(false));
+            }
+        }else{
+            //register
+            dispatch(setLoading(true));
+        const user = {Fullname, email, password};
+      try {
+        const res = await axios.post(`${API_END_POINT}/register`,user,{
+            headers:{
+                'Content-Type':'application/json'
+            },
+            withCredentials:true
+            });
+       if(res.data.success){
+            toast.success(res.data.message);
+        }
+        setIsLogin(true);
+      } catch (error) {
+        toast.error(error.response.data.message);
+        console.log(error);
+      } finally{
+            dispatch(setLoading(false));
+      }
+    }
         setFullName("");
         SetEmail("");
         setPassword("");
     }
-
     return (
         <div>
             <Header />
@@ -33,7 +85,7 @@ const Login = () => {
                     }
                     <input value={email} onChange={(e)=>SetEmail(e.target.value)} type='email' placeholder='Email' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white'/>
                     <input value={password} onChange={(e)=>setPassword(e.target.value)} type='password' placeholder='Password' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white'/>
-                    <button className=' bg-red-600 mt-6 p-3 text-white rounded-sm font-medium'>{isLogin?"Login":"Signup"}</button>
+                    <button type='submit' className=' bg-red-600 mt-6 p-3 text-white rounded-sm font-medium'>{`${isLoading ? "loading...":(isLogin?"login":"signup")}`}</button>
                     <p className='text-white mt-2'>{isLogin ? "New to Netflix?" : "Already have an account?"}<span onClick={loginHandler} className='ml-1 text-blue-900 font-medium cursor-pointer'>{isLogin ? "Signup" : "Login"}</span></p>
                 </div>
             </form>
